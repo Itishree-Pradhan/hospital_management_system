63f6e38730eefdaa04ce1c0ea260c583e306ea52
Itishree-Pradhan <itishree826@gmail.com>
2026-05-19 17:25:33 +0530

    Update patient.js

--- hospital-management/src/main/resources/static/patient.js
+++ hospital-management/src/main/resources/static/patient.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://hospital-management-system-ht9d.onrender.com/api";
 
 // Global variables to store data
 let departments = [];
